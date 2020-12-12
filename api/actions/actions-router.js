85b5c477b0f9676b7b7d2837ba347a62a5cd2348
jalvaradoWD <jalvarado1337@lavabit.com>
2020-12-11 18:45:42 -0600

    Finished with the Actions Router.

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,11 +1,12 @@
 // Write your "actions" router here!
 const router = require("express").Router();
-const { get } = require("./actions-model");
+const { get, insert, update, remove } = require("./actions-model");
 
 /**
  * @method GET
  * @route /api/actions
- * @description Sends an array of actions (or an empty array) as the body of the response.
+ * @description
+ * Sends an array of actions (or an empty array) as the body of the response.
  */
 router.get("/", async (req, res) => {
   res.status(200).json(await get());
@@ -14,15 +15,60 @@ router.get("/", async (req, res) => {
 /**
  * @method GET
  * @route /api/actions/:id
- * @description  Sends an action with the given id as the body of the response.
+ * @description
+ * Sends an action with the given id as the body of the response.
  */
 router.get("/:id", async (req, res) => {
   const { id } = req.params;
   return res.status(200).json(await get(id));
 });
 
-// router.post("/", async (req, res) => {});
-// router.put("/", async (req, res) => {});
-// router.delete("/", async (req, res) => {});
+/**
+ * @method POST
+ * @route /api/actions/
+ * @description
+ * Sends the newly created action as the body of the response.
+ */
+router.post("/", async (req, res) => {
+  /**
+   * Request Body Required Fields
+   * =============
+   * project_id
+   * description
+   * notes
+   */
+
+  return res.status(201).json(await insert(req.body));
+});
+
+/**
+ * @method PUT
+ * @route /api/actions/:id
+ * @description
+ * Sends the updated action as the body of the response.
+ */
+router.put("/:id", async (req, res) => {
+  /**
+   * Request Body Required Fields
+   * =============
+   * description
+   * notes
+   * completed
+   */
+  const { id } = req.params;
+
+  return res.status(202).json(await update(id, req.body));
+});
+
+/**
+ * @method DELETE
+ * @route /api/actions/:id
+ * @description
+ * sends no response body
+ */
+router.delete("/:id", async (req, res) => {
+  await remove(req.params.id);
+  res.status(202).json({ messge: "Action deleted" });
+});
 
 module.exports = router;
--- index.js
+++ index.js
@@ -16,6 +16,7 @@ const express = require("express");
 const server = express();
 const PORT = 8080;
 
+server.use(express.json());
 const actionsAPI = require("./api/actions/actions-router");
 // const projectsAPI = require("./api/projects/projects-router");
 
